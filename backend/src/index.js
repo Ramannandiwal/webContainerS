70cc8f1eca3f23ab76a78dbea72ae39c1130e4c9
Ramannandiwal <raman.nandiwal@gmail.com>
2025-01-21 16:59:14 +0530

    done1

--- backend/src/index.js
+++ backend/src/index.js
@@ -78,5 +78,5 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://backend:${PORT}`);
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
+    host:'0.0.0.0',
+    
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin"
